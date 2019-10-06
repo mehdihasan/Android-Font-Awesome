ada43fb068c8c1be3077a9985bea77079335f57d
mehdihasan <email2mehdihasan@gmail.com>
2019-10-06 11:55:08 +0200

    updated to androidx

--- fontawesome/src/main/java/info/androidhive/fontawesome/FontTextView.java
+++ fontawesome/src/main/java/info/androidhive/fontawesome/FontTextView.java
@@ -2,7 +2,7 @@ package info.androidhive.fontawesome;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.v7.widget.AppCompatTextView;
+import androidx.appcompat.widget.AppCompatTextView;
 import android.util.AttributeSet;
 
 public class FontTextView extends AppCompatTextView {
